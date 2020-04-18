5dcdd60aeb7596609919e31cc090bf34f5a4ecf3
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-04-18 18:02:50 -0400

    list of confessions

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
 
 import wlc from "../data/westminster/wlc.json";
 import Blog from "../blog/test.mdx";
 
 import '../styles/index.scss';
 
+const readDir = util.promisify(fs.readdir);
+
+const getData = async () => {
+    const folders = await readDir("./data/");
+    return Promise.all(
+        folders.map(async (folder) => {
+            const contents = await readDir(`./data/${folder}`);
+            return Promise.resolve({
+                folder,
+                contents
+            });
+        })
+    );
+};
+
 export async function getStaticProps(context) {
     // will be passed to the page component as props
+    const confessions = await getData();
     return {
         props: {
-            wlc
+            confessions
         }
     };
 };
 
 const HomePage = ({
-    wlc
+    confessions
 }) => {
-    // console.log("data", wlc);
+    console.log(confessions);
     return (
-        <>
-            <h1 className="text-center">Hello Guyz</h1>
-            <Blog />
-        </>
+        <div className="home">
+            <h1 className="text-center">Confessional Christianity</h1>
+            <ul>
+                {confessions.map((confession) => {
+                    return (
+                        <li>
+                            <span>{confession.folder}</span>
+                            <ul>
+                                {confession.contents.map((confession) => <li>{confession}</li>)}
+                            </ul>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
 }
 
