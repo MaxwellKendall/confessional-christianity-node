e81ea8fdd61f62754891207935d1390b0e06cea3
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-04-18 14:57:01 -0400

    tailwind config + json imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-const HomePage = () => {
-    
-    return (
+import '../styles/index.scss';
+import wlc from "../data/westminster/wlc.json";
 
+export async function getStaticProps(context) {
+    return {
+      props: {
+          wlc
+      }, // will be passed to the page component as props
+    }
+}
+
+const HomePage = ({
+    wlc
+}) => {
+    console.log("data", wlc);
+    return (
+        <h1 className="text-center">Hello Guyz</h1>
     );
 }
 
