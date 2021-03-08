98c17f8a01d62aa8ce8f5a0e00bbf6a8319a62cd
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-03-08 16:08:30 -0500

    preview imgs

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -8,10 +8,10 @@ const SEO = ({
   <Head>
     <title>{title}</title>
     <meta property="og:title" content={title} key="title" />
-    <meta property="og:image" content="/preview-img.png" />
+    <meta property="og:image" content="https://confessionalchristianity.com/preview-img.png" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:domain" value="confessionalchristianity.com" />
-    <meta property="twitter:image" content="/preview-img.png" />
+    <meta property="twitter:image" content="https://confessionalchristianity.com/preview-img.png" />
     <link rel="shortcut icon" href="/favicon.ico" />
   </Head>
 );
