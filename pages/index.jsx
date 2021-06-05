508516a311d1047cb2099df6f0097b9a4b9697d8
Maxwell Kendall <Maxwell.N.Kendall@gmail.com>
2021-06-05 15:35:29 -0400

    fixing responsive bug w/ # of matches per document

--- pages/index.jsx
+++ pages/index.jsx
@@ -292,7 +292,7 @@ const HomePage = ({
             <li>
               <h2 className="text-3xl lg:text-4xl w-full mb-24 flex flex-wrap text-center">
                 {documentTitle}
-                <span className="text-xl lg:text-lg my-auto mx-auto md:ml-auto md:mr-0">
+                <span className="text-xl lg:text-lg my-auto mx-auto mt-4 2xl:mt-0 2xl:ml-auto 2xl:mr-0">
                   {`${results.length} ${results.length === 1 ? 'MATCH' : 'MATCHES'}`}
                   <FontAwesomeIcon
                     className="ml-5 my-auto text-xl lg:text-lg"
