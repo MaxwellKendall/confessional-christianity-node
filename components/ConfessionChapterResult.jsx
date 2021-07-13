4d2ebd6693884d2d47e91bbf0bbc022e8afdd116
Maxwell Kendall <Maxwell.N.Kendall@gmail.com>
2021-07-12 23:06:07 -0400

    feature: Nav via Chapter for WCF/HC/CD
    Still need to implement on ConfessionalTextResult
    for BC, WSC, and WLC

--- components/ConfessionChapterResult.jsx
+++ components/ConfessionChapterResult.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
@@ -5,21 +6,18 @@ import React from 'react';
 import { uniqueId } from 'lodash';
 import Highlighter from 'react-highlight-words';
 import Link from 'next/link';
-
-import { excludedWordsInDocumentId } from '../dataMapping';
-import ConfessionTextResult from './ConfessionTextResult';
-import { getConfessionalAbbreviationId } from '../scripts/helpers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const generateLink = (docId, chapterId) => {
-  return {
-    pathname: '/',
-    query: {
-      search: `document:${docId} chapter:${chapterId}`,
-    },
-  };
-};
+import ConfessionTextResult from './ConfessionTextResult';
+import { getConfessionalAbbreviationId } from '../scripts/helpers';
+
+const generateLink = (docId, chapterId) => ({
+  pathname: '/',
+  query: {
+    search: `document:${docId} chapter:${chapterId}`,
+  },
+});
 
 const ConfessionChapterResult = ({
   docId = null,
@@ -39,11 +37,11 @@ const ConfessionChapterResult = ({
   const renderNav = () => [{ direction: 1, show: hasNext }, { direction: -1, show: hasPrevious }]
     .filter(({ show }) => show)
     .map((obj) => (
-      <li className={obj.direction > 0 ? 'absolute top-2.5 left-full' : 'absolute top-2.5 right-full'}>
-        <Link href={generateLink(docId, chapterIdAsInt + obj.direction)} className="relative left-full">
+      <li className={obj.direction > 0 ? 'absolute top-0 left-full ml-2 lg:ml-5' : 'absolute top-0 right-full mr-2 lg:mr-5'}>
+        <Link scroll={false} href={generateLink(docId, chapterIdAsInt + obj.direction)} className="relative left-full">
           {obj.direction > 0
-            ? <FontAwesomeIcon className="cursor-pointer" icon={faChevronRight} />
-            : <FontAwesomeIcon className="cursor-pointer" icon={faChevronLeft} />}
+            ? <FontAwesomeIcon className="cursor-pointer" icon={faChevronRight} size="xs" />
+            : <FontAwesomeIcon className="cursor-pointer" icon={faChevronLeft} size="xs" />}
         </Link>
       </li>
     ));
@@ -51,24 +49,21 @@ const ConfessionChapterResult = ({
   return (
     <li key={uniqueId()} className={`w-full flex flex-col justify-center mb-24 ${showNav ? ' absolute' : ''}`}>
       <>
-        {docId && chapterId && searchTerms.length > 0 && (
-          <Link href={generateLink(docId, chapterId)}>
-            <>
-              <Highlighter className="cursor-pointer header text-3xl lg:text-4xl w-full text-center mb-24 uppercase" textToHighlight={title} searchWords={searchTerms} highlightClassName="search-result-matched-word" />
-              <ul>
-                {renderNav()}
-              </ul>
-            </>
-          </Link>
-        )}
-        {docId && chapterId && !searchTerms && (
-          <Link href={generateLink(docId, chapterId)}>
-            <>
-              <h3 className="cursor-pointer text-3xl lg:text-4xl w-full text-center mb-24">{title}</h3>
-              <ul>
-                {renderNav()}
-              </ul>
-            </>
+        {docId && chapterId && (
+          <Link scroll={false} href={generateLink(docId, chapterId)}>
+            <a className="cursor-pointer header text-3xl lg:text-4xl w-full text-center mb-24 uppercase">
+              {searchTerms && (
+                <Highlighter textToHighlight={title} searchWords={searchTerms} highlightClassName="search-result-matched-word" />
+              )}
+              {!searchTerms && (
+                <h3>{title}</h3>
+              )}
+              {showNav && (
+                <ul>
+                  {renderNav()}
+                </ul>
+              )}
+            </a>
           </Link>
         )}
         {!docId && !chapterId && !searchTerms && (
