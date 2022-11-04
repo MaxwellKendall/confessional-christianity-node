99573fbc47b5f15f5d0d0268e9d2d64580b0c518
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2022-11-04 15:28:49 -0400

    add regex for bible

--- helpers/index.js
+++ helpers/index.js
@@ -158,6 +158,7 @@ const wildCardFacetRegex = new RegExp(/\*/);
 const removeDot = (str) => str && str.replaceAll('.', '');
 export const regexV2 = /(wcf|Westminster\sConfession\sof\sFaith|hc|Heidelberg\sCatechism|WSC|Westminster\sShorter\sCatechism|WLC|Westminster\sLarger\sCatechism|39A|Thirty Nine Articles|39 Articles|tar|bcf|bc|Belgic Confession of Faith|Belgic Confession|COD|CD|Canons of Dordt|95T|95 Theses|Ninety Five Theses|ML9T|all|\*)|(\1\.[0-9]{1,})|(\1\2\.[0-9]{1,})/ig;
 export const keyWords = /(westminster\sstandards|three\sforms\sof\sunity|3\sforms\sof\sunity|six\sforms\sof\sunity|6\sforms\sof\sunity)/ig;
+export const bibleRegex = /(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|ruth|1\ssamuel|2\ssamuel|1\skings|2\skings|1\schronicles|2\schronicles|ezra|nehemiah|esther|job|psalms|psalm|proverbs|ecclesiastes|song\sof\ssolomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|testament|matthew|mark|luke|john|acts|romans|1\scorinthians|2\scorinthians|galatians|ephesians|philippians|colossians|1\sthessalonians|2\sthessalonians|1\stimothy|2\stimothy|titus|philemon|hebrews|james|1\speter|2\speter|1\sjohn|2\sjohn|3\sjohn|jude|revelation)|(\1\s[0-9]{1,})|(\1\s[0-9]{1,}:[0-9]{1,})/ig;
 
 // 2d array is like an OR
 export const parseFacets = (str) => {
@@ -253,7 +254,7 @@ export const parseFacets = (str) => {
 };
 
 export const getPgTitle = (search) => {
-  const queryWithoutFacetFilters = (search && `${search.replace(regexV2, '').replace(keyWords, '')}`) || null;
+  const queryWithoutFacetFilters = (search && `${search.replace(regexV2, '').replace(keyWords, '')}`).replace(bibleRegex, '') || null;
   if (!search) return ['Search the Confessions of Historic Protestantism', startCase(queryWithoutFacetFilters)];
   const result = search.match(regexV2);
   const doc = (result && result.length && getCanonicalDocId(result[0])) || null;
@@ -270,6 +271,12 @@ export const getPgTitle = (search) => {
   }
   if (keyWords.test(search)) {
     return [`The ${startCase(search.match(keyWords)[0].toLowerCase())}`, startCase(queryWithoutFacetFilters)];
+  }
+  if (bibleRegex.test(search)) {
+    return [
+      startCase(search.match(bibleRegex).join(' ').toLowerCase()),
+      startCase(queryWithoutFacetFilters),
+    ];
   }
   return [queryWithoutFacetFilters, ''];
 };
